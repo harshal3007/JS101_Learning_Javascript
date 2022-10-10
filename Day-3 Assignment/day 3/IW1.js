fb425fc6b6308cc157b1147d5343bd27da361375
harshal3007 <harshalbhawsar9@gmail.com>
2022-10-10 17:17:11 +0530

    Day 3

---
+++ Day-3 Assignment/day 3/IW1.js
@@ -0,0 +1,7 @@
+
+let num= 15;
+
+if (num%3==0) {
+  console.log("multiple if 3")
+}
+  
---
+++ Day-3 Assignment/day 3/IW4.js
@@ -0,0 +1,17 @@
+
+let userdata_user= "harshalwww";
+let userdata_pass= 1000;
+
+let user= "harshlwww";
+let pass=1000;
+
+if (userdata_user==user) {
+    if (userdata_pass==pass) {
+      console.log("Login succesful")
+    }else {
+      console.log("Incorrect password")
+    }
+}else {
+  console.log("Incorrect username")
+}
+   
